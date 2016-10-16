471276f86fdb63cd9588a2fee0d21aa8f4820992
Vinicius Biondi <viniciusvwxyz@gmail.com>
2016-10-16 16:28:33 -0200

    tentando arrumar o sendFile

--- SystemBuildingControl/src/br/com/cco2anpi/views/SendFileView.java
+++ SystemBuildingControl/src/br/com/cco2anpi/views/SendFileView.java
@@ -15,6 +15,8 @@ import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.ResourceBundle;
 
+import javax.swing.DefaultListModel;
+import javax.swing.DefaultListSelectionModel;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JList;
@@ -24,10 +26,13 @@ import javax.swing.JScrollPane;
 import javax.swing.ListModel;
 import javax.swing.ListSelectionModel;
 import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableModel;
 
 import org.bouncycastle.crypto.DataLengthException;
 import org.bouncycastle.crypto.InvalidCipherTextException;
 
+import br.com.cco2anpi.clients.UserClient;
+import br.com.cco2anpi.models.User;
 import br.com.cco2anpi.tools.Crypto;
 import br.com.cco2anpi.tools.FileHandler;
 import br.com.cco2anpi.views.shared.CustomButton;
@@ -49,13 +54,15 @@ public class SendFileView extends JPanel {
 	private JScrollPane rightScrollPane;
 
 	private String[] data;
+	private String[] data2;
 
-	private CustomButton toLeftButton;
+	private CustomButton removeSelectedButton;
 	private CustomButton toRightButton;
-	private CustomButton allToLeftButton;
+	private CustomButton removeAllButton;
 	private CustomButton allToRightButton;
 	private CustomButton confirmButton;
 	private GridBagConstraints gbc = new GridBagConstraints();
+	private JLabel lbl ;
 
 	ResourceBundle bn;
 	/**
@@ -70,17 +77,18 @@ public class SendFileView extends JPanel {
 		joinButtonsPanel = new JPanel();
 		joinListsPanel = new JPanel();
 		joinAllPanel = new JPanel();
-
+		leftPanel = new JPanel(new BorderLayout());
+		rightPanel = new JPanel(new BorderLayout(5, 5));
 		leftList = new JList();
 		rightList = new JList();
 
 		leftScrollPane = new JScrollPane();
 		rightScrollPane = new JScrollPane();
 
-		toLeftButton = new CustomButton(bn.getString("mever_selecionados"));
-		toLeftButton.addActionListener(new ButtonListener());
-		allToLeftButton = new CustomButton(bn.getString("mover_todos"));
-		allToLeftButton.addActionListener(new ButtonListener());
+		removeSelectedButton = new CustomButton(bn.getString("remover_selecionados"));
+		removeSelectedButton.addActionListener(new ButtonListener());
+		removeAllButton = new CustomButton(bn.getString("remover_todos"));
+		removeAllButton.addActionListener(new ButtonListener());
 		toRightButton = new CustomButton(bn.getString("mever_selecionados"));
 		toRightButton.addActionListener(new ButtonListener());
 		allToRightButton = new CustomButton(bn.getString("mover_todos"));
@@ -93,54 +101,32 @@ public class SendFileView extends JPanel {
 		buildLeftPanel();
 		buildRightPanel();
 		buildListsPanel();
-//		joinAllPanel();
+
 
 		setBackground(new Color(255, 255, 255));
 		setLayout(new GridBagLayout());
-		JLabel lbl = new JLabel(bn.getString("acesso_a_usuarios"), SwingConstants.CENTER);
-		// JFields
+		lbl = new JLabel(bn.getString("acesso_a_usuarios"), SwingConstants.CENTER);
+
 		gbc.gridx = 0;
 		gbc.gridy = 0;
-//		gbc.gridwidth = 3;
-//		gbc.fill = GridBagConstraints.HORIZONTAL;
 		gbc.insets = new Insets(0, 0, 50, 0);
 		add(lbl, gbc);
 
-		// CustomButtons
 		gbc.gridx = 0;
 		gbc.gridy = 1;
-//		gbc.gridwidth = 3;
 		gbc.insets = new Insets(0, 0, 0, 0);
 		add(joinButtonsPanel, gbc);
 
-		// JTable
 		gbc.gridx = 0;
 		gbc.gridy = 2;
-//		gbc.gridwidth = 3;
 		gbc.insets = new Insets(10, 0, 10, 0);
 		add(joinListsPanel, gbc);
 		
-		// JTable
 		gbc.gridx = 0;
 		gbc.gridy = 3;
-//		gbc.gridwidth = 3;
 		gbc.insets = new Insets(10, 0, 100, 0);
 		add(confirmButton, gbc);
-		
-				
-		
-//		add(lbl, BorderLayout.NORTH);
-//		add(joinAllPanel, BorderLayout.CENTER);
-
 	}
-
-//	private void joinAllPanel() {
-//		joinAllPanel.setLayout(new BorderLayout(2, 2));
-//		joinAllPanel.add(joinButtonsPanel, BorderLayout.NORTH);
-//		joinAllPanel.add(joinListsPanel, BorderLayout.CENTER);
-//		joinAllPanel.add(confirmButton, BorderLayout.SOUTH);
-//	}
-
 	/**
 	 * Create a grid with the buttons to send data of leftList to the rightList
 	 */
@@ -159,8 +145,8 @@ public class SendFileView extends JPanel {
 	private void buildButtonRightPanel() {
 
 		buttonRightPanel.setLayout(new GridLayout(1, 3));
-		buttonRightPanel.add(toLeftButton);
-		buttonRightPanel.add(allToLeftButton);
+		buttonRightPanel.add(removeSelectedButton);
+		buttonRightPanel.add(removeAllButton);
 		buttonRightPanel.add(new JLabel(""));
 		buttonRightPanel.setBackground(new Color(255, 255, 255));
 	}
@@ -179,25 +165,32 @@ public class SendFileView extends JPanel {
 	/**
 	 * The buildLeftPanel method list all user of system in a JList.
 	 */
-	private void buildLeftPanel() {
-		leftPanel = new JPanel(new BorderLayout());
-		data = new String[11];
-		for (int i = 0; i < data.length; i++) {
+	public void buildLeftPanel() {
+		
+		
+		System.out.println("chegei em buildLeftPanel");
+		
+		data = new String[UserClient.getAllUsers().getBody().length];
+		
+		int i = 0;
+		for (User user : UserClient.getAllUsers().getBody()) {
+			
 			String salt = "";
 			try {
 				salt = Crypto.generateRandomSalt();
 			} catch (UnsupportedEncodingException e1) {
-				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}
 			String password = "";
 			try {
-				password = Crypto.encrypt("password", salt);
+				password = Crypto.encrypt(user.getPassword(), salt);
 			} catch (DataLengthException | IllegalStateException | InvalidCipherTextException e) {
 				e.printStackTrace();
 			}
-			data[i] = "username" + i + " " + salt + " " + password + "\n";
+			data[i] = user.getName() + " " + salt + " " + password + "\n";
+			i++;
 		}
+
 		leftList = new JList<String>(data);
 		leftList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
 		leftList.setVisibleRowCount(6);
@@ -208,10 +201,25 @@ public class SendFileView extends JPanel {
 	/**
 	 * This method make a panel to show all users that has access to building
 	 */
-	private void buildRightPanel() {
-		rightPanel = new JPanel(new BorderLayout(5, 5));
-		rightList = new JList();
+	private void buildRightPanel() {	
+		
+		System.out.println("chegei em buildRightPanel");
+
+		String content = "";
+		Path currentRelativePath = Paths.get("");
+		String s = currentRelativePath.toAbsolutePath().toString();
+		
+		try {
+			content = FileHandler.read(s, "login.txt");
+
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		String[] sub = content.split("\n");
+		
+		rightList = new JList<String>(sub);
 		rightList.setVisibleRowCount(6);
+		leftList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
 		rightScrollPane = new JScrollPane(rightList);
 		rightPanel.add(rightScrollPane);
 	}
@@ -219,7 +227,8 @@ public class SendFileView extends JPanel {
 	/**
 	 * Join lists in one new panel
 	 */
-	private void buildListsPanel() {
+	public void buildListsPanel() {		
+
 		joinListsPanel.setLayout(new GridLayout(1, 2));
 		joinListsPanel.add(leftPanel);
 		joinListsPanel.add(rightPanel);
@@ -231,13 +240,13 @@ public class SendFileView extends JPanel {
 	private class ButtonListener implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
 
-			if (e.getSource() == allToLeftButton) {
+			if (e.getSource() == removeAllButton) {
 				ListModel model = leftList.getModel();
 				sendList = new String[model.getSize()];
 				for (int i = 0; i < model.getSize(); i++) {
 					sendList[i] = ((String) model.getElementAt(i));
 				}
-				System.out.println(Arrays.toString(sendList));
+				System.out.println(Arrays.toString(sendList)+"\nDevem ser removidos");
 
 			}
 			if (e.getSource() == allToRightButton) {
@@ -247,13 +256,17 @@ public class SendFileView extends JPanel {
 					sendList[i] = ((String) model.getElementAt(i));
 				}
 				JOptionPane.showMessageDialog(null, "Valores selecionados: \n" + Arrays.toString(sendList));
-				System.out.println(Arrays.toString(sendList));
+				
 			}
-			if (e.getSource() == toLeftButton) {
-
+			if (e.getSource() == removeSelectedButton) {
+				int index=rightList.getSelectedIndex();
+				DefaultListModel listModel2 = new DefaultListModel();
+				listModel2.addElement(rightList.getSelectedValue());
+				rightList.remove(index);
+				System.out.println(Arrays.toString(listModel2.toArray())+"\nDevem ser removidos");
 			}
 			if (e.getSource() == toRightButton) {
-
+				System.out.println(Arrays.toString(sendList)+"\nDevem ser removidos");
 			}
 			if (e.getSource() == confirmButton) {
 				String content = "";
@@ -274,15 +287,15 @@ public class SendFileView extends JPanel {
 		}
 	}
 
+	/**
+	 * Update language of all literal components of this panel  
+	 * */
 	public void updateLanguage(ResourceBundle bn) {
-		toLeftButton.setText(bn.getString("mever_selecionados"));
-		
-		allToLeftButton.setText(bn.getString("mover_todos"));
-		
+		removeSelectedButton.setText(bn.getString("remover_selecionados"));
+		removeAllButton.setText(bn.getString("remover_todos"));
 		toRightButton.setText(bn.getString("mever_selecionados"));
-		
-		allToRightButton.setText(bn.getString("mover_todos"));
-		
+		allToRightButton.setText(bn.getString("mover_todos"));		
 		confirmButton.setText(bn.getString("confirmar"));		
+		lbl.setText(bn.getString("acesso_a_usuarios"));
 	}
 }
