57b255de68555a4a286d226d70e92b700d844486
Giovanni Maciel <Giovanni Maciel@DESKTOP-PTU5R5M>
2017-02-26 19:17:20 -0300

    Inserção de testes

---
+++ Services/src/br/com/cco2anpi/services/controller/tests/UserControllerTest.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package br.com.cco2anpi.services.controller.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Giovanni Maciel
+ *
+ */
+public class UserControllerTest {
+
+	/**
+	 * Test method for {@link br.com.cco2anpi.services.controllers.UserController#getAllUsers()}.
+	 */
+	@Test
+	public void testGetAllUsers() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link br.com.cco2anpi.services.controllers.UserController#getUser(br.com.cco2anpi.models.User)}.
+	 */
+	@Test
+	public void testGetUser() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link br.com.cco2anpi.services.controllers.UserController#insert(br.com.cco2anpi.models.User)}.
+	 */
+	@Test
+	public void testInsert() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link br.com.cco2anpi.services.controllers.UserController#update(br.com.cco2anpi.models.User)}.
+	 */
+	@Test
+	public void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link br.com.cco2anpi.services.controllers.UserController#delete(br.com.cco2anpi.models.User)}.
+	 */
+	@Test
+	public void testDelete() {
+		fail("Not yet implemented");
+	}
+
+}
--- Services/src/br/com/cco2anpi/services/controllers/UserController.java
+++ Services/src/br/com/cco2anpi/services/controllers/UserController.java
@@ -27,78 +27,78 @@ import br.com.cco2anpi.models.User;
 @RequestMapping("User/*")
 public class UserController extends BaseController {
 
-    /**
-     * Method used to get all users
-     * 
-     * @return array of users
-     */
-    @RequestMapping(value = "getAllUsers", method = RequestMethod.GET)
-    public @ResponseBody ResponseEntity<User[]> getAllUsers() {
-	IUser[] usersDB = userRepository.getAllUsers();
-	User[] users = new User[usersDB.length];
-	for (int i = 0; i < users.length; i++) {
-	    users[i] = new User(usersDB[i]);
+	/**
+	 * Method used to get all users
+	 * 
+	 * @return array of users
+	 */
+	@RequestMapping(value = "getAllUsers", method = RequestMethod.GET)
+	public @ResponseBody ResponseEntity<User[]> getAllUsers() {
+		IUser[] usersDB = userRepository.getAllUsers();
+		User[] users = new User[usersDB.length];
+		for (int i = 0; i < users.length; i++) {
+			users[i] = new User(usersDB[i]);
+		}
+		return new ResponseEntity<User[]>(users, HttpStatus.OK);
 	}
-	return new ResponseEntity<User[]>(users, HttpStatus.OK);
-    }
 
-    /**
-     * Method used to get user
-     * 
-     * @param user
-     *            object user
-     * @return user filled
-     */
-    @RequestMapping(value = "getUser", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-    public @ResponseBody ResponseEntity<User> getUser(@RequestBody User user) {
-	if (userRepository.exists(user)) {
-	    return new ResponseEntity<User>(new User(userRepository.getUser(user.getId())), HttpStatus.OK);
+	/**
+	 * Method used to get user
+	 * 
+	 * @param user
+	 *            object user
+	 * @return user filled
+	 */
+	@RequestMapping(value = "getUser", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody ResponseEntity<User> getUser(@RequestBody User user) {
+		if (userRepository.exists(user)) {
+			return new ResponseEntity<User>(new User(userRepository.getUser(user.getId())), HttpStatus.OK);
+		}
+		return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
 	}
-	return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
-    }
 
-    /**
-     * Method used to insert user
-     * 
-     * @param user
-     *            to be inserted
-     * @return user filled
-     */
-    @RequestMapping(value = "insert", method = RequestMethod.POST)
-    public @ResponseBody ResponseEntity<User> insert(@RequestBody User user) {
-	if (userRepository.exists(user)) {
-	    return new ResponseEntity<User>(HttpStatus.CONFLICT);
+	/**
+	 * Method used to insert user
+	 * 
+	 * @param user
+	 *            to be inserted
+	 * @return user filled
+	 */
+	@RequestMapping(value = "insert", method = RequestMethod.POST)
+	public @ResponseBody ResponseEntity<User> insert(@RequestBody User user) {
+		if (userRepository.exists(user)) {
+			return new ResponseEntity<User>(HttpStatus.CONFLICT);
+		}
+		return new ResponseEntity<User>(new User(userRepository.insert(user)), new HttpHeaders(), HttpStatus.CREATED);
 	}
-	return new ResponseEntity<User>(new User(userRepository.insert(user)), new HttpHeaders(), HttpStatus.CREATED);
-    }
 
-    /**
-     * Method used to update user
-     * 
-     * @param user
-     *            to be updated
-     * @return user updated
-     */
-    @RequestMapping(value = "update", method = RequestMethod.POST)
-    public @ResponseBody ResponseEntity<User> update(@RequestBody User user) {
-	if (userRepository.exists(user)) {
-	    return new ResponseEntity<User>(new User(userRepository.update(user)), new HttpHeaders(), HttpStatus.OK);
+	/**
+	 * Method used to update user
+	 * 
+	 * @param user
+	 *            to be updated
+	 * @return user updated
+	 */
+	@RequestMapping(value = "update", method = RequestMethod.POST)
+	public @ResponseBody ResponseEntity<User> update(@RequestBody User user) {
+		if (userRepository.exists(user)) {
+			return new ResponseEntity<User>(new User(userRepository.update(user)), new HttpHeaders(), HttpStatus.OK);
+		}
+		return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
 	}
-	return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
-    }
 
-    /**
-     * Method used to delete user
-     * 
-     * @param user
-     *            to be deleted
-     * @return status
-     */
-    @RequestMapping(value = "delete", method = RequestMethod.POST)
-    public @ResponseBody ResponseEntity<Boolean> delete(@RequestBody User user) {
-	if (userRepository.exists(user)) {
-	    return new ResponseEntity<Boolean>(userRepository.delete(user), new HttpHeaders(), HttpStatus.NO_CONTENT);
+	/**
+	 * Method used to delete user
+	 * 
+	 * @param user
+	 *            to be deleted
+	 * @return status
+	 */
+	@RequestMapping(value = "delete", method = RequestMethod.POST)
+	public @ResponseBody ResponseEntity<Boolean> delete(@RequestBody User user) {
+		if (userRepository.exists(user)) {
+			return new ResponseEntity<Boolean>(userRepository.delete(user), new HttpHeaders(), HttpStatus.NO_CONTENT);
+		}
+		return new ResponseEntity<Boolean>(HttpStatus.NOT_FOUND);
 	}
-	return new ResponseEntity<Boolean>(HttpStatus.NOT_FOUND);
-    }
 }
