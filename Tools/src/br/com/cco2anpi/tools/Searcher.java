71d985ae3040f7e929edc3d74c39c9b686710ea0
Wendel Hime <wendelhime@hotmail.com>
2017-03-12 17:35:06 -0300

    Teste com IntelliJ IDEA, remoção de warnings

--- Tools/src/br/com/cco2anpi/tools/Searcher.java
+++ Tools/src/br/com/cco2anpi/tools/Searcher.java
@@ -11,17 +11,19 @@ public class Searcher {
 
 	public static int binarySearch(Object[] a, Object key)
 	{
-		return binarySearch0(a, 0, a.length, key);
+		return binarySearch(a, 0, a.length, key);
 	}
 	
-	private static int binarySearch0(Object[] a, int fromIndex, int toIndex, Object key) {
+	private static <T> int binarySearch(Object[] a, int fromIndex, int toIndex, Object key) {
 		int low = fromIndex;
 		int high = toIndex - 1;
 
 		while (low <= high) {
 			int mid = (low + high) >>> 1;
-			Comparable midVal = (Comparable) a[mid];
-			int cmp = midVal.compareTo(key);
+			@SuppressWarnings("unchecked")
+			Comparable<T> midVal = (Comparable<T>) a[mid];
+			@SuppressWarnings("unchecked")
+			int cmp = midVal.compareTo((T) key);
 
 			if (cmp < 0)
 				low = mid + 1;
