62bcc54f5f0add2718cb669b115b17d3a314e5a1
Vinicius Biondi <viniciusvwxyz@gmail.com>
2017-03-20 00:46:12 -0300

    test up webapp

--- SystemBuildingControlWeb/src/br/com/cco2anpi/systemBuildingControlWeb/controllers/UserController.java
+++ SystemBuildingControlWeb/src/br/com/cco2anpi/systemBuildingControlWeb/controllers/UserController.java
@@ -14,12 +14,19 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
  */
 //   http://localhost:8080/SystemBuildingControlWeb
 @Controller
-@EnableWebMvc
-@RequestMapping("User/*")
+//@EnableWebMvc
+//@RequestMapping("User/*")
 public class UserController extends BaseController {
 	
-	@RequestMapping("index")
-	public ModelAndView index(){
-		return new ModelAndView("/User/user");
+//	@RequestMapping("index")
+//	public ModelAndView index(){
+//		return new ModelAndView("/User/user");
+//	}
+	@RequestMapping("/welcome")
+	public ModelAndView helloWorld() {
+ 
+		String message = "<br><div style='text-align:center;'>"
+				+ "<h3>********** Hello World, Spring MVC Tutorial</h3>This message is coming from CrunchifyHelloWorld.java **********</div><br><br>";
+		return new ModelAndView("welcome", "message", message);
 	}
 }
